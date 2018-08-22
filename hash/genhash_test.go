2a8b53776918184f7c64b6975462db0b33491363
Rebecca DiBari <rdibari1@gmail.com>
2018-08-21 20:52:49 -0600

    failing test and a new hash file

---
+++ hash/genhash_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestGenHash(t *testing.T) {
+     hash := generate_hash("angryMonkey")
+     if hash != "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q==" {
+       t.Errorf("Hash was incorrect. got %s, want ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q== ", hash)
+     }
+}
